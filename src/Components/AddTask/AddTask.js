40ed8bbc51dfff8d1fb7cf9e34a4404a42717011
habib <mdhabib71024@gmail.com>
2022-05-19 02:50:48 +0600

    almost done

--- src/Components/AddTask/AddTask.js
+++ src/Components/AddTask/AddTask.js
@@ -6,8 +6,21 @@ const AddTask = () => {
     const formSubmit =event=>{
         event.preventDefault();
         const name=event.target.name.value;
-        const descrip=event.target.descrip.value;
-        console.log(name ,descrip);
+        const about=event.target.descrip.value;
+     
+
+        const url=`http://localhost:5000/users`;
+        fetch(url,{
+
+            method:'POST',
+            headers:{
+                
+                'content-type':'application/json'
+            },
+            body:JSON.stringify({name ,about})
+        })
+        .then(res=>res.json())
+        .then(data=>{console.log(data)})
 
 
     }
@@ -17,10 +30,10 @@ const AddTask = () => {
                 <h3>Add New Task</h3>
                 <br />
            <form onSubmit={formSubmit} className=''>
-               <input className='input-css' name='name' placeholder='Name' type="text" />
+               <input className='input-css' required name='name' placeholder='Name' type="text" />
                <br />
                <br />
-               <input className='input-css' name='descrip' placeholder='description' type="text" />
+               <input className='input-css' required name='descrip' placeholder='description' type="text" />
                <br /><br />
                <input className='btn new-submit ' type="submit"  value='complete'/>
            </form>
--- src/Components/MyTask/MyTask.js
+++ src/Components/MyTask/MyTask.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './task.css';
 
 const MyTask = () => {
+    const [Task,setTask]=useState([]);
+    useEffect(()=>{
+        fetch('http://localhost:5000/users')
+        .then(res=>res.json())
+        .then(data=>setTask(data))
+    },[]);
+
+    const taskdelete=id=>{
+        
+        const url=`http://localhost:5000/users/${id}`;
+        fetch(url,{
+            method:'DELETE'
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            
+          const remain= Task.filter(task=> task._id !== id)
+          setTask(remain);
+         
+            
+        });
+        
+
+    }
+
     return (
-        <div>
-            <h1>My task</h1>
+        <div className='text-center my-5'>
+            <h1> Task</h1>
+            {
+                Task.map(task=>
+                
+                <div className='text-center'>
+                <div className='info-css'>
+                    <div  className='m-5 divs' >
+                <h4>Name : {task.name}</h4>
+                <h6>{task.about}</h6>
+                </div>
+                <button onClick={()=>taskdelete(task._id)} className='btn delete-css'>Delete</button>
+    
+                </div>
+                
+                
+            </div>
+            )
+            }
+
          
             
         </div>
